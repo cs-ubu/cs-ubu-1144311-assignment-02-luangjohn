9cb9003a527a7ba406a980377a959d6a8a6adc2e
luangjohn <athip.si.60@ubu.ac.th>
2019-07-18 14:19:47 +0700

    Elimination+Back

--- la.py
+++ la.py
@@ -1,9 +1,36 @@
+#!/usr/bin/env python3
+import numpy as np
 from mat import *
 
 A = readm('A.csv')
 b = readm('b.csv')
 
 def solve(A, b):
-    # YOUR CODE HERE
-    # x = []
-    return x
+    A,b = np.array(A), np.array(b)
+    # 1. elimination
+    n = len(A[0])
+    x = np.array([0]*n)
+    # print(f'n={n}')
+    for k in range(n-1): # pivot eq #k
+        # print(f'เลือกสมการที่ {k}')
+        for j in range(k+1, n): # pivot eq #j
+            # print(f'กำจัดตัวแปรที่ {k},ออกจากสมการที่ {j}')
+            lam = A[j][k]/A[k][k]
+            # update A[j][k] เป็นต้นไป
+            A[j,k+1:n] = A[j, k+1:n] - lam*A[k,k+1:n]
+            # print(f'\t\tlambda = {lam}')
+            # update b[j]
+            b[j] = b[j] - lam*b[k]
+        # printm(A)  
+    # 2. Back Substitution
+    for k in range(n-1,-1,-1):
+        # print(f'back sub k={k}')
+        x[k] = (b[k] - np.dot(A[k,k+1:n], x[k+1:n]))/A[k,k]
+
+    return x.flatten()
+
+print("=== A ===")
+print(A)
+print("=== B ===")
+print(b)
+print(solve(A,b))
